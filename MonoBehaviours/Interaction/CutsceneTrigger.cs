f845ff4ffe5e6467080ad9dfdeab66b717ba9de4
raevskiy <raevskiy-s-u@yandex.ru>
2018-10-22 23:33:42 +0300

    New script for launching cutscenes

---
+++ MonoBehaviours/Interaction/CutsceneTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace KopliSoft.Interaction
+{
+    public class CutsceneTrigger : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject target;
+        [SerializeField]
+        private PlayableDirector playableDirector;
+
+        private bool cutscenePlayed;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!cutscenePlayed && other.gameObject == target)
+            {
+                playableDirector.enabled = true;
+                cutscenePlayed = true;
+            }
+        }
+
+    }
+}
